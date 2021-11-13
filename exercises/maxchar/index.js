d96ce935b1baabb0964a896ed26b93f245c7497b
Charles Ybarra <charles.ybarra@outlook.com>
2021-11-13 14:52:55 -0600

    maxChar algo practice

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,29 +6,25 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    //iterate through chars of string
-    //if key is not assigned in a let result =[]
-    //assign key value pairs
-
     let values = {};
     let max = 0;
-    let maxValue = ''
+    let maxChar = '';
 
-    for(let char of str){
-        if(!values[char]){
-            values[char] = 1
+    for (let char of str) {
+        if (!values[char]) {
+            values[char] = 1;
         }
         else values[char]++
-        console.log(values)
     }
 
-    for(let char in values){
-        if(values[char] > max){
-            max = values[char]
-            maxValue = char
-        } 
+    for (let char in values) {
+        if (values[char] > max) {
+            max = values[char];
+            maxChar = char;
+        }
     }
-    return maxValue
+
+    return maxChar
 }
 
 module.exports = maxChar;
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -12,3 +12,30 @@ test('Finds the most frequently used char', () => {
 test('Works with numbers in the string', () => {
   expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
 });
+
+
+/*
+    //iterate through chars of string
+    //if key is not assigned in a let result =[]
+    //assign key value pairs
+
+    let values = {};
+    let max = 0;
+    let maxValue = ''
+
+    for(let char of str){
+        if(!values[char]){
+            values[char] = 1
+        }
+        else values[char]++
+        console.log(values)
+    }
+
+    for(let char in values){
+        if(values[char] > max){
+            max = values[char]
+            maxValue = char
+        } 
+    }
+    return maxValue
+*/
