32ba16f7c12b996655557a462616a832da3646fc
Charles Ybarra <charles.ybarra@outlook.com>
2021-12-01 15:53:22 -0600

    stack practice

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -11,8 +11,38 @@
 //   s.pop(); // returns 1
 
 class Stack {
+    //stack is first in, last out
+    
+    //[first, second, third, fourth] == [bottom <=> top]
 
+    constructor(){
+        this.data = [];
+    }
+
+    push(el){
+        this.data.push(el);
+    }
+
+    pop(){
+        return this.data.pop();
+    }
+
+    peek(){
+        let last = this.data[this.data.length - 1]
+        return last;
+    }
 }
+
+s = new Stack
+
+s.push('first')
+s.push('second')
+s.push('third')
+console.log(s,'should log first second and third')
+
+s.pop()
+console.log(s, 'should remove third element')
+s.peek() //should return 'second'
 
 
 module.exports = Stack;
