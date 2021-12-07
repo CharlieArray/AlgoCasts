c969a02f0abb410cdd0c12b0d637bfe5180d1a6d
Charles Ybarra <charles.ybarra@outlook.com>
2021-12-06 20:32:48 -0600

    maxChar algo practice

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,16 +7,29 @@
 
 function maxChar(str) {
    //count values by key:value pairs
+    let object = {}
 
-    //let items = {};
+    for(let item of str){
+        if(!object[item]) {
+            object[item] = 1
+            }
+        else object[item]++
+        }
 
-    //for(let element of str)
-        //if(!items[element])
-            //items[element] = 1 
-        //else items[element]++
-    
-    //
+    //object ex = {a: 1, b:1, c: 6}    
 
+    let maxValue = 0;
+    let maxChar = "" 
+
+    //find key with largest value 
+        //iterate through object
+        for(let item in object){
+            if(object[item] > maxValue){
+                maxValue = object[item];
+                maxChar = item;
+            }
+        }
+    return maxChar;
 }
 
 module.exports = maxChar;
