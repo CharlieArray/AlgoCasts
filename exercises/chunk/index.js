efc6a8e294601cb7d510484a3042e0c85b854698
Charles Ybarra <charles.ybarra@outlook.com>
2021-12-07 01:39:40 -0600

    chunked arr practice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,16 +8,16 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
+function chunk(chunk, size){
     let chunked = [];
 
-    //iterate through array
-    for(let element of array){
-        let last = chunked[chunked.length - 1]
-        if(!last || last.length == size){
-            chunked.push([element])
-        } 
-        else last.push(element)
+    for(let element of chunk){
+        let last = chunked[chunked.length - 1];
+
+        if(!last || last.length === size){
+            chunked.push([element]);
+          }
+        else last.push(element);
     }
     return chunked;
 }
