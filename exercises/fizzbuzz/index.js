ae8e80a222cb333ee9f28cf9755137f02efff9ab
Charles Ybarra <charles.ybarra@outlook.com>
2022-01-23 00:11:04 -0600

    fizz prac

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,17 +13,18 @@
 //   buzz
 
 function fizzBuzz(n) {
-    //console log nums from 1 to n
 
     for(let i = 1; i <= n; i++){
-        if(i % 15 === 0) console.log('fizzbuzz')
 
-        else if(i % 3 === 0) console.log('fizz')
+        if(i % 15 == 0)console.log("fizzbuzz");
 
-        else if(i % 5 === 0) console.log('buzz')
+        else if(i % 5 == 0)console.log("buzz");
 
-        else console.log(i)
+        else if(i % 3 == 0)console.log("fizz");
+        
+        else console.log(i);
     }
+
 }
 
 module.exports = fizzBuzz;
