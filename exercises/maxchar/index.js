cbc6ba9bef987455bb5aed2f664d9afb9210a907
Charles Ybarra <charles.ybarra@outlook.com>
2022-01-23 00:00:29 -0600

    algo prac for maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,28 +7,32 @@
 
 
 function maxChar(str) {
-    //return most commonly used string
-    let object = {};
+    //first task is create an object of key: value pairs; with values being the # of occurances
+        //{a:0,b:2,c:5,d:2}
+        let values = {};
 
-    for(let char of str){
-        if(!object[char]){
-            object[char] = 1;
+        for(let item of str){
+            if(!values[item]){
+                values[item] = 1;
             }
-        else object[char]++;
-    }
+            else values[item]++;
+        };
 
+
+        //create baseline variables to compare values object 
         let maxValue = 0;
         let maxChar = "";
 
-    for(let char in object){
-        if(object[char] > maxValue){
-            maxValue = object[char];
-            maxChar = char;
-        }
-    }
+        //iterate through object to find max
+        for(let item in values){
+            if(values[item] > maxValue){
+                maxValue = values[item];
+                maxChar = item;
+            }
+        };
 
     return maxChar;
-        
+
 }
 
 module.exports = maxChar;
