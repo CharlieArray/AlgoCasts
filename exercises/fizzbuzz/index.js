7f6760501de65fd0101fdde437c1156f42e3c508
Charles <charles@getbackd.com>
2022-02-09 02:35:18 -0600

    daily algo prac

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -14,17 +14,16 @@
 
 function fizzBuzz(n) {
 
+    //print from 1 to n
     for(let i = 1; i <= n; i++){
+        if(i % 15 == 0)console.log("fizzbuzz")
 
-        if(i % 15 == 0)console.log("fizzbuzz");
+        else if(i % 3 == 0)console.log("fizz");
 
         else if(i % 5 == 0)console.log("buzz");
 
-        else if(i % 3 == 0)console.log("fizz");
-        
         else console.log(i);
     }
-
 }
 
 module.exports = fizzBuzz;
